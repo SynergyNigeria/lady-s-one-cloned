c0452b87c48c9ccc93702b0e81d17e1cda093688
SynergyNigeria <synergynigeria10@gmail.com>
2026-05-31 18:37:12 +0100

    fixed legacy error

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)"
 API_URL="${SITE_INFO_API_URL:-http://127.0.0.1:8001/api/site-info/}"
 
-cat > config.js <<EOF
+cat > "$SCRIPT_DIR/config.js" <<EOF
 window.siteInfoApiUrl = "$API_URL";
 EOF
